a3fc985f848eb9fe571fc67e0bce9ba0ebd22ba4
Linus Svahn <respons29@gmail.com>
2019-10-25 14:30:36 +0200

    fixed handle method

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,22 +12,22 @@ import javafx.stage.Stage;
 public class Main extends Application {
     GridPane gridScene = new GridPane();
     ActionEvent buttonswitch = new ActionEvent();
-    Button btn1 = new Button();
-    Button btn2 = new Button();
-    Button btn3 = new Button();
-    Button btn4 = new Button();
-    Button btn5 = new Button();
-    Button btn6 = new Button();
-    Button btn7 = new Button();
-    Button btn8 = new Button();
-    Button btn9 = new Button();
-    Button btn10 = new Button();
-    Button btn11 = new Button();
-    Button btn12 = new Button();
-    Button btn13 = new Button();
-    Button btn14 = new Button();
-    Button btn15 = new Button();
-    Button btn16 = new Button();
+    Button btn1 = new Button("1");
+    Button btn2 = new Button("2");
+    Button btn3 = new Button("3");
+    Button btn4 = new Button("4");
+    Button btn5 = new Button("5");
+    Button btn6 = new Button("6");
+    Button btn7 = new Button("7");
+    Button btn8 = new Button("8");
+    Button btn9 = new Button("9");
+    Button btn10 = new Button("10");
+    Button btn11 = new Button("11");
+    Button btn12 = new Button("12");
+    Button btn13 = new Button("13");
+    Button btn14 = new Button("14");
+    Button btn15 = new Button("15");
+    Button btn16 = new Button("TOM");
 
 
 
@@ -51,22 +51,6 @@ public class Main extends Application {
         btn14.setMinSize(100,100);
         btn15.setMinSize(100,100);
         btn16.setMinSize(100,100);
-        btn1.setText("1");
-        btn2.setText("2");
-        btn3.setText("3");
-        btn4.setText("4");
-        btn5.setText("5");
-        btn6.setText("6");
-        btn7.setText("7");
-        btn8.setText("8");
-        btn9.setText("9");
-        btn10.setText("10");
-        btn11.setText("11");
-        btn12.setText("12");
-        btn13.setText("13");
-        btn14.setText("14");
-        btn15.setText("15");
-        btn16.setText("tom!");
         gridScene.add(btn1, 0,0);
         gridScene.add(btn2, 1,0);
         gridScene.add(btn3, 2,0);
@@ -83,42 +67,49 @@ public class Main extends Application {
         gridScene.add(btn14, 1,3);
         gridScene.add(btn15, 2,3);
         gridScene.add(btn16, 3,3);
-//        btn8.setOnAction((EventHandler<ActionEvent>) buttonswitch, btn8);
-//        btn7.setOnAction(this::handle);
+        btn1.setOnAction(this::handle);
+        btn2.setOnAction(this::handle);
+        btn3.setOnAction(this::handle);
+        btn4.setOnAction(this::handle);
+        btn5.setOnAction(this::handle);
+        btn6.setOnAction(this::handle);
+        btn7.setOnAction(this::handle);
+        btn8.setOnAction(this::handle);
+        btn9.setOnAction(this::handle);
+        btn10.setOnAction(this::handle);
+        btn11.setOnAction(this::handle);
+        btn12.setOnAction(this::handle);
+        btn13.setOnAction(this::handle);
+        btn14.setOnAction(this::handle);
+        btn15.setOnAction(this::handle);
         Scene scene = new Scene(gridScene, 460,460);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(scene);
         primaryStage.show();
 
     }
+    public void handle(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        System.out.println(gridScene.getColumnIndex(button));
+        if(gridScene.getColumnIndex(btn16) == gridScene.getColumnIndex(button) - 1) {
+            gridScene.setColumnIndex(btn16, gridScene.getColumnIndex(button));
+            gridScene.setColumnIndex(button, gridScene.getColumnIndex(button) - 1);
+        }
+        else if(gridScene.getColumnIndex(btn16) == gridScene.getColumnIndex(button) + 1) {
+            gridScene.setColumnIndex(btn16, gridScene.getColumnIndex(button));
+            gridScene.setColumnIndex(button, gridScene.getColumnIndex(button) + 1);
+        }
+        else if(gridScene.getRowIndex(btn16) == gridScene.getRowIndex(button) + 1) {
+            gridScene.setRowIndex(btn16, gridScene.getRowIndex(button));
+            gridScene.setRowIndex(button, gridScene.getRowIndex(button) + 1);
+        }
+        else if(gridScene.getRowIndex(btn16) == gridScene.getRowIndex(button) - 1) {
+            gridScene.setRowIndex(btn16, gridScene.getRowIndex(button));
+            gridScene.setRowIndex(button, gridScene.getRowIndex(button) - 1);
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-    public void bSwitch(ActionEvent buttonswitch, Button button) {
-
-        System.out.println(gridScene.getColumnIndex(btn7));
-        if(gridScene.getColumnIndex(btn16) == gridScene.getColumnIndex(button) - 1) {
-            gridScene.setColumnIndex(btn16, gridScene.getColumnIndex(btn8));
-            gridScene.setColumnIndex(btn8, gridScene.getColumnIndex(btn8) - 1);
-        }
-        else if(gridScene.getColumnIndex(btn16) == gridScene.getColumnIndex(btn8) + 1) {
-            gridScene.setColumnIndex(btn16, gridScene.getColumnIndex(btn8));
-            gridScene.setColumnIndex(btn8, gridScene.getColumnIndex(btn8) - 1);
-        }
-    }
-
-//    @Override
-//    public void handle(Event event) {
-//        System.out.println(gridScene.getColumnIndex(btn7));
-//        if(gridScene.getColumnIndex(btn16) == gridScene.getColumnIndex(btn8) - 1) {
-//            gridScene.setColumnIndex(btn16, gridScene.getColumnIndex(btn8));
-//            gridScene.setColumnIndex(btn8, gridScene.getColumnIndex(btn8) - 1);
-//        }
-//        else if(gridScene.getColumnIndex(btn16) == gridScene.getColumnIndex(btn8) + 1) {
-//            gridScene.setColumnIndex(btn16, gridScene.getColumnIndex(btn8));
-//            gridScene.setColumnIndex(btn8, gridScene.getColumnIndex(btn8) - 1);
-//        }
-//    }
 }
